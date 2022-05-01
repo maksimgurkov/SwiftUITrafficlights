1d9603383e63712198b421bca21177721478d957
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-01 18:29:46 +0300

    Initial Commit

---
+++ SwiftUITrafficlights/SwiftUITrafficlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficlightsApp.swift
+//  SwiftUITrafficlights
+//
+//  Created by Максим Гурков on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
